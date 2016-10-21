85531caee76dad9d41fa822ac85d9926b431be6a
bencode <bencode@163.com>
2016-10-21 08:18:08 +0800

    for react

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ exports = module.exports = require('eslint-config-bce');
 Object.assign(exports, {
   parser: 'babel-eslint',
 
+  plugins: [
+    'react'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
   env: {
-    node:true,
     browser: true,
-    es6: true
-  }
+    amd: true,
+    es6: true,
+    node: true,
+    mocha: true
+  },
+
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
 });
 
 
 Object.assign(exports.rules, {
   // 方便import 语句对齐
-  'no-multi-spaces': 0
+  'no-multi-spaces': 0,
+
+  // react
+  'react/prop-types': 1,
+  'react/display-name': 1
 });
